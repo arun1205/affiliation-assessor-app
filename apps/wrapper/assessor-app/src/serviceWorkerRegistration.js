bcfeb0325421dc61f608fb932599bf3b978ddb04
devendra <devendra@devendras-MacBook-Pro.local>
2023-08-17 11:11:18 +0530

    PWA config changes

--- apps/wrapper/assessor-app/src/serviceWorkerRegistration.js
+++ apps/wrapper/assessor-app/src/serviceWorkerRegistration.js
@@ -34,7 +34,7 @@ export function register(config) {
 
     window.addEventListener('load', () => {
       console.log(process.env.PUBLIC_URL)
-      const swUrl = `${process.env.PUBLIC_URL}/serviceWorker.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
