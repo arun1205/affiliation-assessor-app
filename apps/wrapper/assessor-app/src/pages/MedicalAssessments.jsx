148e6dde43cfac8db80655e28b796ff0824fe84a
Arun Nair <arun.kumar@tarento.com>
2023-12-19 12:54:09 +0530

    ui fixes

--- apps/wrapper/assessor-app/src/pages/MedicalAssessments.jsx
+++ apps/wrapper/assessor-app/src/pages/MedicalAssessments.jsx
@@ -130,7 +130,7 @@ const MedicalAssessments = () => {
       pageTitle="Today's Inspection"
     >
       <div
-        className={`flex flex-col px-6 min-h-[calc(100vh-214px)] overflow-y-scroll pb-6 ${
+        className={`flex flex-col px-6 h-3/4 overflow-y-scroll pb-6 ${
           !data?.id ? "justify-center" : ""
         }`}
       >
