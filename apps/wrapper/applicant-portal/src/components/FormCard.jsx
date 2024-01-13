58e9105028346b3c35cc3f8e32796fdee9c1270a
Arun Nair <arun.kumar@tarento.com>
2024-01-13 14:40:06 +0530

    added last date subnmission date

--- apps/wrapper/applicant-portal/src/components/FormCard.jsx
+++ apps/wrapper/applicant-portal/src/components/FormCard.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import { Card, Button } from "./index";
+import { readableDate } from "../utils/common";
+import {  Tooltip } from "@material-tailwind/react";
 
 const FormCard = (props) => {
   let name = props?.form?.course_name;
   name = name.charAt(0).toUpperCase() + name.slice(1);
+
+
+  const isFormSubmissionDateOver = () => {
+    //if (new Date("2023-11-11") < new Date()) {
+    if (new Date(props?.form?.form?.last_submission_date) < new Date()) {
+      return true;
+    } else {
+      return false
+    }
+  }
+
 
   return (
     <Card moreClass="flex flex-col border-gray-100 m-3 gap-3 w-[300px] border-[1px] drop-shadow justify-between">
@@ -28,6 +41,16 @@ const FormCard = (props) => {
         <span className={"text-teal-400 text-sm rounded-md p-1"} style={{ backgroundColor: "#eee" }}>
           Round : {props?.form?.round}
         </span>
+           
+      </div>
+      <div className="flex" >
+      <span
+              className={`text-xs py-1 px-2  rounded-md text-yellow-800}`}
+              style={{ backgroundColor: "#eee" }}
+            >
+              {console.log(props?.form)}
+            Last Date for Submission: <span>{readableDate(props?.form?.form?.last_submission_date)}</span>
+            </span> 
       </div>
       <div className="flex">
         <Button
@@ -35,7 +58,16 @@ const FormCard = (props) => {
           style={{ backgroundColor: "#fff" }}
           text="Apply"
           onClick={props.onApply ? () => props.onApply(props?.form) : null}
+          otherProps={{
+            disabled : isFormSubmissionDateOver()
+          }}
         ></Button>
+         {  isFormSubmissionDateOver() && 
+            <div className="flex grow gap-4 justify-end items-center">
+         <Tooltip arrow content="Last date to apply is over. Please contact admin">
+                    &#9432;
+                    </Tooltip>
+                    </div>}
       </div>
     </Card>
   );
